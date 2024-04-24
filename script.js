2a42d1dc3d0c3e1d38469cc67156538c5e15a447
MingyangWang-Tom <mingyang.wang97@gmail.com>
2024-04-24 11:46:19 -1000

    remove unnecessary comments

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function addCourse() {
     const container = document.getElementById('coursesContainer');
     const newCourse = container.firstElementChild.cloneNode(true);
-    newCourse.querySelector('.grade').selectedIndex = 0; // Reset grade selection to the first option
-    newCourse.querySelector('.credits').value = ''; // Clear the credits input
+    newCourse.querySelector('.grade').selectedIndex = 0; 
+    newCourse.querySelector('.credits').value = ''; 
     container.appendChild(newCourse);
     updateCourseLabels();
 }
@@ -91,26 +91,18 @@ function processInputs(form) {
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Get the modal
     var disclaimerModal = document.getElementById("disclaimerModal");
-
-    // Get the button that opens the modal
     var disclaimerLink = document.getElementById("disclaimerLink");
-
-    // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks on the button, open the modal
     disclaimerLink.onclick = function() {
         disclaimerModal.style.display = "block";
     }
 
-    // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         disclaimerModal.style.display = "none";
     }
-
-    // When the user clicks anywhere outside of the modal, close it
+    
     window.onclick = function(event) {
         if (event.target == disclaimerModal) {
             disclaimerModal.style.display = "none";
