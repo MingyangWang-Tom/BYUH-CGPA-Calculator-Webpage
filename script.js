b69cad55fbacb66931c766efce2352f0e77ee201
MingyangWang-Tom <mingyang.wang97@gmail.com>
2024-04-24 11:44:26 -1000

    fix the footer and add disclaimer

--- script.js
+++ script.js
@@ -89,3 +89,31 @@ function processInputs(form) {
     document.getElementById('result').textContent = `Your CGPA is: ${finalCGPA}`;
     document.getElementById('semGPA').textContent = `Your semester GPA is: ${currentGPA}`;
 }
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Get the modal
+    var disclaimerModal = document.getElementById("disclaimerModal");
+
+    // Get the button that opens the modal
+    var disclaimerLink = document.getElementById("disclaimerLink");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    disclaimerLink.onclick = function() {
+        disclaimerModal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        disclaimerModal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == disclaimerModal) {
+            disclaimerModal.style.display = "none";
+        }
+    }
+});
